e8f8874daa41b024d22a823dabca7848cbdc0540
Fouru <hiroshi.kaze1994@gmail.com>
2018-07-21 14:06:09 +0700

    1

--- worldeditor.bat
+++ worldeditor.bat
@@ -1,4 +1,4 @@
-asdasd@ECHO OFF
+@ECHO OFF
 cls
 copy "C:\git\Together-We-Stand\OneDrive\map\Together We Stand.w3x" "C:\git\Together-We-Stand\temp\temp.w3x"
 "C:\Users\hiros\OneDrive\Desktop\SharpCraft World Editor Extended Bundle\SharpCraft World Editor Extended Bundle\World Editor Extended.exe" -loadfile "C:\git\Together-We-Stand\temp\temp.w3x"
