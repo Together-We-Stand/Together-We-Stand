ce8a5dde9b8120038dd51fab1840d3d13d4ab07c
Fouru <hiroshi.kaze1994@gmail.com>
2018-07-21 13:55:35 +0700

    setup folder

--- OneDrive/bats/run WEX.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-cls
-copy "C:\git\Together-We-Stand\OneDrive\map\Together We Stand.w3x" "C:\git\Together-We-Stand\OneDrive\map\temp\temp.w3x"
-"C:\Users\hiros\OneDrive\Desktop\SharpCraft World Editor Extended Bundle\SharpCraft World Editor Extended Bundle\World Editor Extended.exe" -loadfile "C:\git\Together-We-Stand\OneDrive\map\temp\temp.w3x"
